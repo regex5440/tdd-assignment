df14587bd8fa09281996e7cf01802e122251f4d8
Harsh Dagar <amandagar000@gmail.com>
2024-07-28 12:47:43 +0530

    refractor

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,27 +1,27 @@
 import add from "../src";
 
 describe("Handle invalid input", () => {
-  it("empty string", (d) => {
-    expect(add("")).toBe(0);
-    d();
-  });
   it("non-string input", (d) => {
     expect(add(1)).toBe(0);
+    d();
+  });
+  it("multiple alphanumeric values", (d) => {
+    expect(add.bind(null, "1,ab,3s")).toThrow(TypeError);
     d();
   });
 });
 
 describe("Handle valid input", () => {
+  it("empty string", (d) => {
+    expect(add("")).toBe(0);
+    d();
+  });
   it("single digit", (d) => {
     expect(add("1")).toBe(1);
     d();
   });
   it("multiple digits", (d) => {
     expect(add("1,2,3")).toBe(6);
-    d();
-  });
-  it("multiple alphanumeric values", (d) => {
-    expect(add.bind(null, "1,ab,3s")).toThrow(TypeError);
     d();
   });
   it("multiple digits with comma and whitespace", (d) => {
