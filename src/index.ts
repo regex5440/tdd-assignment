b28cb9d757651b6cdfdbf43c8547019d42157672
Harsh Dagar <amandagar000@gmail.com>
2024-07-28 16:05:03 +0530

    support custom delimiter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 export default function add(inputString: any): number {
   if (typeof inputString !== "string" || inputString.length === 0) return 0;
+  const customSeparator = inputString.match(/^\/\/(.)\n/);
+  let separator = ",\n\\s";
+  let splittedString = inputString;
 
+  if (customSeparator) {
+    separator += `\\${customSeparator[1]}`;
+    splittedString = inputString.split("\n")[1];
+  }
+
+  const regex = new RegExp(`[${separator}]+`);
   let sum = 0;
-  for (const values of inputString.split(/[,\n\s]+/)) {
+
+  for (const values of splittedString.split(regex)) {
     const num = parseInt(values);
     if (isNaN(num)) {
       throw new TypeError("Alphanumeric values are not allowed");
