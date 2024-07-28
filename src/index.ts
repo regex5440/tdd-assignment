73c6de2fef8ec261142d3992775cb6ae0a147c3c
Harsh Dagar <amandagar000@gmail.com>
2024-07-28 12:45:01 +0530

    support newline additionally as separator

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export default function add(inputString: any): number {
   if (typeof inputString !== "string" || inputString.length === 0) return 0;
 
   let sum = 0;
-  for (const values of inputString.split(",")) {
+  for (const values of inputString.split(/[,\n\s]+/)) {
     const num = parseInt(values);
     if (isNaN(num)) {
       throw new TypeError("Alphanumeric values are not allowed");
